be6ce95f6192ac97c565369e2f51c216d57e0d3d
mgstabrani <mgstabrani19@gmail.com>
2021-05-31 23:09:32 +0700

    Add put method

--- src/handler.js
+++ src/handler.js
@@ -83,4 +83,56 @@ const getBookByIdHandler = (request, h) => {
     }
 }
 
-module.exports = { saveBookHandler, getAllBooksHandler, getBookByIdHandler };
+const editBookByIdHandler = (request, h) => {
+    const {bookId} = request.params;
+
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = books.findIndex((book) => book.id === bookId);
+
+    if(name == null){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    };
+
+    if(readPage > pageCount){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    };
+
+    if(index !== -1){
+        books[index] = {
+            ...books[index],
+            name, year, author, summary, publisher, pageCount, readPage, reading, updatedAt,
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }else{
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Id tidak ditemukan',
+            data: {
+                index,
+                bookId,
+            }
+        });
+        response.code(404);
+        return response;
+    }
+}
+
+module.exports = { saveBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { saveBookHandler, getAllBooksHandler, getBookByIdHandler } = require("./handler");
+const { saveBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler } = require("./handler");
 
 const routes = [
     {
@@ -15,6 +15,11 @@ const routes = [
         method: 'GET',
         path: '/books/{bookId}',
         handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler : editBookByIdHandler,
     }
   ];
    
